0e47f7ddb10f7adbcb44b848598cdca2045abfb2
Rob Hicks <robert.hicks@familysearch.org>
2022-12-16 16:20:42 -0700

    add tiny-encryption-algorithm and change jsonResponse to return entire response

--- packages/tiny-fetch/src/interceptors/json-response.js
+++ packages/tiny-fetch/src/interceptors/json-response.js
@@ -1,15 +1,16 @@
 const jsonResponse = {
   response(response) {
-    if (response.status === 204 || response.status === 201) return JSON.stringify({})
+    if (response.status === 204 || response.status === 201)
+      return JSON.stringify({ headers: response.headers, status: response.status })
     return response.text().then((text) => {
       try {
-        return JSON.parse(text)
+        return { headers: response.headers, status: response.status, data: JSON.parse(text) }
       } catch (err) {
         return text
       }
     })
   },
-  id: "TINY_FETCH_JSON_RESPONSE",
+  id: 'TINY_FETCH_JSON_RESPONSE',
 }
 
 export default jsonResponse
